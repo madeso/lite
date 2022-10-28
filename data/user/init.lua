09bfcb1cd60ebe499455c15eb4ec9699573fe28d
Gustav <sir.gustav.the.coder@gmail.com>
2022-10-28 21:18:35 +0200

    added solarized light as color

--- data/user/init.lua
+++ data/user/init.lua
@@ -7,6 +7,7 @@ local style = require "core.style"
 
 -- light theme:
 -- require "user.colors.summer"
+require "user.colors.solarized_light"
 
 -- key binding:
 -- keymap.add { ["ctrl+escape"] = "core:quit" }
