19521482c310c68939536ec61b14819f4a961b7c
Gustav <sir.gustav.the.coder@gmail.com>
2022-11-19 15:44:38 +0100

    feat: don't highlight empty spaces

--- data/plugins/selectionhighlight.lua
+++ data/plugins/selectionhighlight.lua
@@ -20,16 +20,19 @@ function DocView:draw_line_body(idx, x, y)
   if line1 == line2 and col1 ~= col2 then
     local lh = self:get_line_height()
     local selected_text = self.doc.lines[line1]:sub(col1, col2 - 1)
-    local current_line_text = self.doc.lines[idx]
-    local last_col = 1
-    while true do
-      local start_col, end_col = current_line_text:find(selected_text, last_col, true)
-      if start_col == nil then break end
-      local x1 = x + self:get_col_x_offset(idx, start_col)
-      local x2 = x + self:get_col_x_offset(idx, end_col + 1)
-      local color = style.selectionhighlight or style.syntax.comment
-      draw_box(x1, y, x2 - x1, lh, color)
-      last_col = end_col + 1
+    selected_text = selected_text:gsub("%s", "")
+    if #selected_text ~= 0 then
+        local current_line_text = self.doc.lines[idx]
+        local last_col = 1
+        while true do
+            local start_col, end_col = current_line_text:find(selected_text, last_col, true)
+            if start_col == nil then break end
+            local x1 = x + self:get_col_x_offset(idx, start_col)
+            local x2 = x + self:get_col_x_offset(idx, end_col + 1)
+            local color = style.selectionhighlight or style.syntax.comment
+            draw_box(x1, y, x2 - x1, lh, color)
+            last_col = end_col + 1
+        end
     end
   end
   draw_line_body(self, idx, x, y)
